dc5fc34f70605ba6d8fea4bfc8088d1b4b92548e
Sadana <tushar.169107181@muj.manipal.edu>
2018-07-29 15:26:04 +0530

    Email done tepmlate needs to be added, REplicating Recycelrview and CSV needs to be done

--- app/src/main/java/tech/sadana/mujconnectadmin/RecyclerViewAdapter.java
+++ app/src/main/java/tech/sadana/mujconnectadmin/RecyclerViewAdapter.java
@@ -1,7 +1,9 @@
 package tech.sadana.mujconnectadmin;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -42,29 +44,56 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     }
 
     @Override
-    public void onBindViewHolder(final ViewHolder holder, int position) {
+    public void onBindViewHolder(final ViewHolder holder, final int position) {
         final DatabaseReference mDatabase;
 // ...
         FirebaseAuth mAuth = FirebaseAuth.getInstance();
         FirebaseUser user = mAuth.getCurrentUser();
         String email = user.getEmail().toString();
-        String club = new String ();
-        if(email.equals("mujaperture@gmail.com")) {
+        String club = new String();
+        final String subject;
+        String body;
+        if (email.equals("mujaperture@gmail.com")) {
             club = "Aperture";
+            subject= "Registration Successful for Aperture";
             mDatabase = FirebaseDatabase.getInstance().getReference("aperture");
-        }
-        else{
+        } else {
             mDatabase = null;
             club = null;
+            subject = null;
         }
         final Details studentDetails = MainImageUploadInfoList.get(position);
 
-        holder.StudentNameTextView.setText(studentDetails.getFirstName() +" "+studentDetails.getLastName() );
+        holder.StudentNameTextView.setText(studentDetails.getFirstName() + " " + studentDetails.getLastName());
         final String localReg = studentDetails.getRegNum();
         holder.StudentNumberTextView.setText(studentDetails.getRegNum());
-        DatabaseReference checkPaid = mDatabase.child(localReg.toString()).child("paid");
+        String checkPaid = MainImageUploadInfoList.get(position).getPaid().toString();
+        if (checkPaid.equals("true")) {
 
-        final String finalClub = club;
+
+            holder.Pay.setVisibility(View.GONE);
+            holder.StudentNameTextView.setTextColor(Color.parseColor("#96c93d"));
+            holder.StudentNumberTextView.setTextColor(Color.parseColor("#96c93d"));
+//            holder.layoutMain.setOnLongClickListener(new View.OnLongClickListener() {
+//                @Override
+//                public boolean onLongClick(View view) {
+//
+//                    mDatabase.child(localReg.toString()).child("paid").setValue("false");
+//                    MainImageUploadInfoList.get(position).setPaid("false");
+//
+//
+//
+//
+//                    return false;
+//                }
+//            });
+
+
+
+
+        } else{
+
+            final String finalClub = club;
         holder.Pay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -77,18 +106,24 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
                 holder.StudentNumberTextView.setTextColor(Color.parseColor("#96c93d"));
 
                 try {
-                    GMailSender sender = new GMailSender("elicit2018@gmail.com", "tushar123");
-                    sender.sendMail("Your Reciept for Registration in "+ finalClub,
-                            "You have successfully paid for the Registration and is completed",
-                            "elicit2018@gmail.com",
-                            studentDetails.getEmail().toString());
+                    Intent intent = new Intent(Intent.ACTION_SENDTO);
+                    intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+                    intent.putExtra(Intent.EXTRA_EMAIL, new String[] {studentDetails.getEmail()});
+                    intent.putExtra(Intent.EXTRA_SUBJECT,subject );
+                    intent.putExtra(Intent.EXTRA_TEXT,"Hi");
+
+                    context.startActivity(intent);
+
+
+
                 } catch (Exception e) {
-                    Toast.makeText(view.getContext(),"Email not sent",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(view.getContext(), "Email not sent", Toast.LENGTH_SHORT).show();
                 }
 
 
             }
         });
+    }
 
     }
 
@@ -106,6 +141,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         public TextView StudentNameTextView;
         public TextView StudentNumberTextView;
         public Button Pay;
+        public RelativeLayout layoutMain;
 
         public ViewHolder(View itemView) {
 
@@ -115,6 +151,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
             StudentNumberTextView =  itemView.findViewById(R.id.detailreg);
             Pay = itemView.findViewById(R.id.payButton);
+            layoutMain = itemView.findViewById(R.id.mainRel);
         }
     }
 }
